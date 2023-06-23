cfd8b706993d7eacb0c18a8d3f084af1b28516e0
Surya Prasad Parajapati <Ercssurya2018@gmail.com>
2023-06-23 17:46:36 +0530

    whatshapp
    whatshapp

---
+++ IdentityTable/Controllers/WhatshopNotificationController.cs
@@ -0,0 +1,37 @@
+﻿using IdentityTable.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatsAppApi;
+
+namespace IdentityTable.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhatshopNotificationController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendMessage(WhatshopModel model)
+        {
+            try
+            {
+                string from = "6386513958";
+                WhatsApp wa = new WhatsApp(from, "863802068773501/73", model.Name);
+                wa.OnConnectSuccess += () =>
+                {
+                    wa.SendMessage(model.MobileNo, model.Message);
+                };
+                return Ok("Sended");
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+            
+        }
+    }
+}
